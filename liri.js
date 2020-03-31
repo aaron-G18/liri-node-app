74e16d609dc5f1ab4eecd314e9c2558157fbcb10
Aaron Garner <aaron.garner.1982@gmail.com>
2020-03-31 12:32:25 -0700

    Got the concert-this and spotify-this-song working.

--- liri.js
+++ liri.js
@@ -5,15 +5,17 @@ var axios = require("axios");
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var argsCopy = Array.from(process.argv)
+var argsCopy2 = Array.from(process.argv)
 
-var args = process.argv.splice(2);
+var args = Array.from(argsCopy.splice(2));
+var args2 = Array.from(argsCopy2.splice(3));
+var search = args2.join(" ");
 
 //  `concert-this`;
-console.log(args);
-
 
 if (args[0] === "concert-this") {
-    var artist = args[1];
+    var artist = search;
     var queryUrl =
         "https://rest.bandsintown.com/artists/" +
         artist +
@@ -21,12 +23,13 @@ if (args[0] === "concert-this") {
 
     axios.get(queryUrl).then(
         function (response) {
-
-            // console.log(response);
-
-            // console.log(response.data[0].venue);
-
+            console.log("*");
+            console.log("*");
+            console.log("*");
             console.log(artist + " events:");
+            console.log("*");
+            console.log("*");
+            console.log("*");
             for (var i in response.data) {
                 console.log("--------------------")
                 console.log("Venue Name: " + response.data[i].venue.name);
@@ -35,33 +38,6 @@ if (args[0] === "concert-this") {
             };
 
         },
-
-        // data: [
-        //     {
-        //       offers: [],
-        //       venue: [Object],
-        //       datetime: '2020-03-30T20:00:00',
-        //       artist: [Object],
-        //       on_sale_datetime: '',
-        //       description: 'Live Stream \n\nhttps://www.youtube.com/user/MetallicaTV/featured',
-        //       lineup: [Array],
-        //       id: '102213303',
-        //       title: 'Metallica Mondays',
-        //       artist_id: '128',
-        //       url: 'https://www.bandsintown.com/e/102213303?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event'
-        //     },
-
-        // {
-        //     name: 'Metallica Mondays',
-        //     country: 'United States',
-        //     region: 'NY',
-        //     city: 'New York City',
-        //     latitude: '40.71427',
-        //     longitude: '-74.00597'
-        //   }
-
-
-
 
         function (error) {
             if (error.response) {
@@ -78,20 +54,139 @@ if (args[0] === "concert-this") {
     );
 }
 
-//  1. `node liri.js concert-this <artist/band name here>`
-
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-//      * Name of the venue
-
-//      * Venue location
-
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-//     * **Important**: There is no need to sign up for a Bands in Town `api_id` key. Use the `codingbootcamp` as your `app_id`. For example, the URL used to search for "Celine Dion" would look like the following:
-
-//       * `https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp`
 
 // `spotify-this-song`
+
+if (args[0] === "spotify-this-song") {
+
+    if (args.length === 1) {
+        console.log("No song was entered. Here is the info for 'The Sign' by Ace of Base.")
+        spotify
+            .search({
+                type: 'track',
+                query: "The Sign"
+            })
+            .then(function (response) {
+                console.log("*");
+                console.log("*");
+                console.log("Results for The Sign");
+                console.log("*");
+                console.log("*");
+                console.log("--------------------")
+                console.log("Artist Name: ", response.tracks.items[0].artists[0].name);
+                console.log("Song Name: ", response.tracks.items[0].name);
+                console.log("Preview Link: ", response.tracks.items[0].preview_url);
+                console.log("Album Name: ", response.tracks.items[0].album.name);
+                console.log("--------------------")
+                console.log("*");
+                console.log("*");
+                console.log("Please enter a song name after 'spotify-this-song' to search. (e.g. node liri.js spotify-this-song The Sign");
+                console.log("*");
+                console.log("*");
+
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+    } else {
+        spotify
+            .search({
+                type: 'track',
+                query: search
+            })
+            .then(function (response) {
+                console.log("*");
+                console.log("*");
+                console.log("Results for ", search);
+                console.log("*");
+                console.log("*");
+                for (var i in response.tracks.items) {
+                    console.log("--------------------")
+                    console.log("Artist Name: ", response.tracks.items[i].artists[0].name);
+                    console.log("Song Name: ", response.tracks.items[i].name);
+                    console.log("Preview Link: ", response.tracks.items[i].preview_url);
+                    console.log("Album Name: ", response.tracks.items[i].album.name);
+                };
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
+
+    };
+}
+
+// response.tracks.items    returns array of objects:
+//    
+// [
+//     {
+//       album: {
+//              album_type: 'compilation',
+//              artists: [Array],
+//              available_markets: [Array],
+//              external_urls: [Object],
+//              href: 'https://api.spotify.com/v1/albums/1Fq1oCtmlSQabl1zIdoWCg',
+//              id: '1Fq1oCtmlSQabl1zIdoWCg',
+//              images: [Array],
+//              name: 'Arista Heritage Series: Ray Parker',
+//              release_date: '2000-02-08',
+//              release_date_precision: 'day',
+//              total_tracks: 11,
+//              type: 'album',
+//              uri: 'spotify:album:1Fq1oCtmlSQabl1zIdoWCg'
+//              },
+//       artists: [ [Object] ],
+//       available_markets: [
+//              'AD', 'AE', 'AR', 'BE', 'BH', 'BO', 'BR', 'CA',
+//              'CH', 'CL', 'CO', 'CR', 'CY', 'DE', 'DK', 'DO',
+//              'DZ', 'EC', 'EE', 'EG', 'ES', 'FI', 'FR', 'GB',
+//              'GR', 'GT', 'HK', 'HN', 'ID', 'IE', 'IN', 'IS',
+//              'IT', 'JO', 'JP', 'KW', 'LB', 'LI', 'LT', 'LU',
+//              'LV', 'MA', 'MC', 'MT', 'MX', 'MY', 'NI', 'NL',
+//               'NO', 'OM', 'PA', 'PE', 'PH', 'PS', 'PT', 'PY',
+//              'QA', 'SA', 'SE', 'SG', 'SV', 'TH', 'TN', 'TW',
+//              'US', 'UY', 'VN', 'ZA'
+//              ],
+//       disc_number: 1,
+//       duration_ms: 240800,
+//       explicit: false,
+//       external_ids: { isrc: 'USAR18400117' },
+//       external_urls: {
+//         spotify: 'https://open.spotify.com/track/300zfRaCgTmEm5Eqe3HqZZ'
+//          },
+//       href: 'https://api.spotify.com/v1/tracks/300zfRaCgTmEm5Eqe3HqZZ',
+//       id: '300zfRaCgTmEm5Eqe3HqZZ',
+//       is_local: false,
+//       name: 'Ghostbusters',
+//       popularity: 61,
+//       preview_url: 'https://p.scdn.co/mp3-preview/fc919929a10a7d712f76c36566321e2048f7daa8?cid=ccdf0875926e459ebdc541cd188992a6',
+//       track_number: 4,
+//       type: 'track',
+//       uri: 'spotify:track:300zfRaCgTmEm5Eqe3HqZZ'
+//     },
+
+
+
+
+// 2. `node liri.js spotify-this-song '<song name here>'`
+
+//    * This will show the following information about the song in your terminal/bash window
+
+//  X    * Artist(s)
+
+//   X   * The song's name
+
+//   X   * A preview link of the song from Spotify
+
+//      * The album that the song is from
+
+//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+
+
+
+
+
 // `movie-this`
 // `do-what-it-says`;
